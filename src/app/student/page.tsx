a6f61ed1486e56560856a21c18b8d2a38f0055a9
Joshclxx <joshuacolobong11@gmail.com>
2025-06-23 11:25:52 +0800

    Create Login Component Create Student Home Page

---
+++ src/app/student/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SectionContainer from "@/components/SectionContainer";
+import Profile from "@/components/student/Profile";
+
+const page = () => {
+  return (
+    <SectionContainer background="mt-1 h-auto">
+      <Profile />
+    </SectionContainer>
+  );
+};
+
+export default page;
